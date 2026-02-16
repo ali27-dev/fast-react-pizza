2cb7d869b2418ba79aef3d7b86f4f0c71ea48dbd
ali27-dev <khanali3229@gmail.com>
2026-02-16 11:31:18 +0500

    For implementing style,I'm using tailwind to use this,First of all i'm install it and setup in my project.

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
+// prettier.config.js
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+};
