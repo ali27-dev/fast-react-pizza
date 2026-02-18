00dcc1c3c683a6f577c136bcaeb3bab846f56695
ali27-dev <khanali3229@gmail.com>
2026-02-18 23:00:15 +0500

    Implement some reusing stylling to buttons with React Components.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,17 @@
-function Button({ children, disabled }) {
+import { Link } from 'react-router-dom';
+function Button({ children, disabled, to }) {
+  const className =
+    'inline-block rounded-full bg-yellow-400 px-4 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 md:py-4';
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      className="inline-block rounded-full bg-yellow-400 px-4 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed"
-      disabled={disabled}
-    >
-      {children}
+    <button className={className} disabled={disabled}>
+      {children} 
     </button>
   );
 }
