02df6a6f78822a2c7b5c82394335bdd536dfcad5
ali27-dev <khanali3229@gmail.com>
2026-02-20 23:13:48 +0500

    After the Styling,Let's implement some dynamic funtionality,So First up of all i'm install react-redux for global state and also implement the username sate functionaly but some part are left.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
+  const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
   return (
     <div>
-      <p className="hidden text-sm font-semibold md:block">Abid</p>
+      <p className="hidden text-sm font-semibold md:block">{userName}</p>
     </div>
   );
 }
