464258c35ee03624fb46eb4f4628b199dab3bbd8
ali27-dev <khanali3229@gmail.com>
2026-02-21 23:53:40 +0500

    Bulid the cart Page and also, Implement the clear cart functionality.

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -11,6 +11,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-semibold">{formatCurrency(totalPrice)}</p>
+
         <Button type="small">Delete</Button>
       </div>
     </li>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -50,6 +50,8 @@ export const {
 
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getAddToCarQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
